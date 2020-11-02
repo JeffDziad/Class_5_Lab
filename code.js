857fecc5204c207f4cedff606c0a5bd37a4e6461
BuildTools <unconfigured@null.spigotmc.org>
2020-11-02 17:08:22 -0600

    Added Second Question, Fixed Javascript and CSS

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
 $(document).ready(function()
 {
     $("form").submit(generateGreeting);
+    $("#qtyLearning").change(updatePrice);
+    $("#qtyDonation").change(updatePrice);
 
     function generateGreeting(event)
     {
@@ -28,5 +30,32 @@ $(document).ready(function()
             $(".output").css("color", "red");
         }
         $(".output").show();
+    }
+
+    function updatePrice()
+    {
+        let qtyLearn = parseFloat($("#qtyLearning").val());
+        let qtyDonate = parseFloat($("#qtyDonation").val());
+        let output = 0;
+
+        if (isNaN(qtyLearn))
+        {
+            $("#totalDonation").text(`$${qtyDonate.toFixed(2) * 14.99}`);
+            output = qtyDonate + qtyLearn;
+            $("#gTotal").text(`$${output.toFixed(2)}`);
+        }
+        else if (isNaN(qtyDonate))
+        {
+            $("#totalLearning").text(`$${qtyLearn.toFixed(2) * 39.99}`);
+            output = qtyDonate + qtyLearn;
+            $("#gTotal").text(`$${output.toFixed(2)}`);
+        }
+        else
+        {
+            $("#totalLearning").text(`$${qtyLearn.toFixed(2) * 39.99}`);
+            $("#totalDonation").text(`$${qtyDonate.toFixed(2) * 14.99}`);
+            output = (qtyLearn * 39.99) + (qtyDonate * 14.99);
+            $("#gTotal").text(`$${output.toFixed(2)}`);
+        }
     }
 });
